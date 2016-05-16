778f3e02964b8f3e509f253e2b3d6fd1bc36593c
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-05-16 18:45:47 +0300

    OOT-1509 parameter instead of hardcoded issue entity class name

--- src/Vitalii/Bundle/TrackerBundle/DependencyInjection/VitaliiTrackerExtension.php
+++ src/Vitalii/Bundle/TrackerBundle/DependencyInjection/VitaliiTrackerExtension.php
@@ -15,6 +15,7 @@ class VitaliiTrackerExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
         $loader->load('form.yml');
     }
 }
