19f4dd234901bf1efaac838e5148c725b42da30b
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-06-09 19:19:34 +0300

    OOT-1503 Functional tests for all actions in issue controller

--- src/Vitalii/Bundle/TrackerBundle/Tests/Functional/ControllersTest.php
+++ src/Vitalii/Bundle/TrackerBundle/Tests/Functional/ControllersTest.php
@@ -18,9 +18,16 @@ class ControllersTest extends WebTestCase
 
     protected function setUp()
     {
-        $this->initClient(array(), $this->generateBasicAuthHeader());
+        $this->initClient(array(), $this->generateBasicAuthHeader(), $force = true);
         $this->doctrine = $this->getContainer()->get('doctrine');
         $this->loadFixtures(['Vitalii\Bundle\TrackerBundle\Tests\Functional\DataFixtures\LoadIssueData']);
+    }
+
+    public function testIndex()
+    {
+        $this->client->request('GET', '/tracker/issue/');
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 200);
     }
 
     public function testView()
@@ -34,10 +41,53 @@ class ControllersTest extends WebTestCase
         $this->assertContains("test-02", $crawler->html());
     }
 
-    public function testIndex()
+    public function testUpdate()
     {
-        $this->client->request('GET', '/tracker/issue/');
+        /** @var Issue $issue */
+        $issue = $this->doctrine->getRepository('VitaliiTrackerBundle:Issue')->findOneByCode('test-02');
+
+        $crawler = $this->client->request('GET', '/tracker/issue/update/' . $issue->getId());
         $result = $this->client->getResponse();
         $this->assertHtmlResponseStatusCodeEquals($result, 200);
+        $this->assertContains("test-02", $crawler->html());
+    }
+
+    public function testCreate()
+    {
+        $this->client->request('GET', '/tracker/issue/create');
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 200);
+    }
+
+    public function testAddSubtaskInStory()
+    {
+        // Issue of type story
+        /** @var Issue $issue */
+        $issueStory = $this->doctrine->getRepository('VitaliiTrackerBundle:Issue')->findOneByCode('test-01');
+
+        $this->client->request('GET', '/tracker/issue/subtask/' . $issueStory->getId());
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 200);
+    }
+
+    public function testAddSubtaskInTask()
+    {
+        // Issue of type task
+        /** @var Issue $issue */
+        $issueTask = $this->doctrine->getRepository('VitaliiTrackerBundle:Issue')->findOneByCode('test-02');
+
+        $this->client->request('GET', '/tracker/issue/subtask/' . $issueTask->getId());
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 403);
+    }
+
+    public function testDelete()
+    {
+        /** @var Issue $issue */
+        $issue = $this->doctrine->getRepository('VitaliiTrackerBundle:Issue')->findOneByCode('test-02');
+
+        $crawler = $this->client->request('GET', '/tracker/issue/delete/' . $issue->getId());
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 302);
     }
 }
