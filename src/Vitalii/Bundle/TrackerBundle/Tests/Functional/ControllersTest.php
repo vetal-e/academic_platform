ad4e5c01d248dc227634f57ead992f3f32b3f125
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-06-09 14:55:27 +0300

    Configured test environment; Simple functional test for the issue index action

--- src/Vitalii/Bundle/TrackerBundle/Tests/Functional/ControllersTest.php
+++ src/Vitalii/Bundle/TrackerBundle/Tests/Functional/ControllersTest.php
@@ -9,8 +9,6 @@ use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
  */
 class ControllersTest extends WebTestCase
 {
-    const TEST_ISSUE_ID = 1;
-
     protected function setUp()
     {
         $this->initClient(array(), $this->generateBasicAuthHeader());
@@ -18,23 +16,8 @@ class ControllersTest extends WebTestCase
 
     public function testIndex()
     {
-        $crawler = $this->client->request('GET', $this->getUrl('tracker.issue_index'));
+        $this->client->request('GET', '/tracker/issue/');
         $result = $this->client->getResponse();
         $this->assertHtmlResponseStatusCodeEquals($result, 200);
-        $this->assertEquals('All Issues', $crawler->filter('#page-title')->html());
     }
-
-//    public function testView()
-//    {
-//        $crawler = $this->client->request(
-//            'GET',
-//            $this->getUrl(
-//                'oro_calendar_view',
-//                array('id' => self::DEFAULT_USER_CALENDAR_ID)
-//            )
-//        );
-//        $result = $this->client->getResponse();
-//        $this->assertHtmlResponseStatusCodeEquals($result, 200);
-//        $this->assertEquals('John Doe - Calendars - John Doe', $crawler->filter('#page-title')->html());
-//    }
 }
