d5943a04986f317c76eac6566326457b564c4a67
v.eremenko <v.eremenko@itftc.com>
2016-06-12 20:49:43 +0300

    OOT-1507 Test for API returning single issue data

--- src/Vitalii/Bundle/TrackerBundle/Tests/Functional/IssueApiTest.php
+++ src/Vitalii/Bundle/TrackerBundle/Tests/Functional/IssueApiTest.php
@@ -5,11 +5,12 @@ namespace Vitalii\Bundle\TrackerBundle\Tests\Functional;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Oro\Bundle\ApiBundle\Request\RequestType;
 use Oro\Bundle\ApiBundle\Tests\Functional\ApiTestCase;
+use Vitalii\Bundle\TrackerBundle\Entity\Issue;
 
 /**
  * @dbIsolation
  */
-class IssueViewSectionsTest extends ApiTestCase
+class IssueApiTest extends ApiTestCase
 {
     /**
      * @var Registry
@@ -33,11 +34,31 @@ class IssueViewSectionsTest extends ApiTestCase
         parent::setUp();
     }
 
-    public function testApi()
+    public function testIssueApiList()
     {
         $this->client->request(
             'GET',
             '/api/trackerissues',
+            [],
+            [],
+            array_replace(
+                $this->generateWsseAuthHeader(),
+                ['CONTENT_TYPE' => 'application/vnd.api+json']
+            )
+        );
+        $response = $this->client->getResponse();
+
+        $this->assertApiResponseStatusCodeEquals($response, 200, 'issue', 'get list');
+    }
+
+    public function testIssueApiSingle()
+    {
+        /** @var Issue $issue */
+        $issue = $this->doctrine->getRepository('VitaliiTrackerBundle:Issue')->findOneByCode('test-02');
+
+        $this->client->request(
+            'GET',
+            '/api/trackerissues/' . $issue->getId(),
             [],
             [],
             array_replace(
