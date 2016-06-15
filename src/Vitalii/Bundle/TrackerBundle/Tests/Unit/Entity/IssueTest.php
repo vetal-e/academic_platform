2f2e6768e9346c78bc54dc0836b75dcb0f049ad9
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-06-15 18:05:35 +0300

    OOT-1501 Unit test for the entity

---
+++ src/Vitalii/Bundle/TrackerBundle/Tests/Unit/Entity/IssueTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Vitalii\Bundle\TrackerBundle\Tests\Unit\Entity;
+
+use Vitalii\Bundle\TrackerBundle\Entity\Issue;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+use Oro\Component\Testing\Unit\EntityTestCaseTrait;
+
+class IssueTest extends \PHPUnit_Framework_TestCase
+{
+    use EntityTestCaseTrait;
+
+    public function testAccessors()
+    {
+        $this->assertPropertyAccessors(new Issue(), [
+            ['id', 42],
+            ['summary', 'some string'],
+            ['code', 'some string'],
+            ['description', 'some string'],
+            ['reporter', new \Oro\Bundle\UserBundle\Entity\User()],
+            ['assignee', new \Oro\Bundle\UserBundle\Entity\User()],
+            ['parentIssue', new Issue()],
+            ['childIssues', new ArrayCollection(), false],
+            ['organization', new \Oro\Bundle\OrganizationBundle\Entity\Organization()],
+            ['createdAt', new \DateTime()],
+            ['updatedAt', new \DateTime()],
+            ['updatedAtSet', 1]
+        ]);
+    }
+}
