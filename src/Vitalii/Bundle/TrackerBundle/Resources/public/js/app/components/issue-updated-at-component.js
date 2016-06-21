07f690c580b43f8dc2f39843140bfc8f73bcfaab
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-06-21 20:07:50 +0300

    reload collaborators grid on the issue page on adding a note

--- src/Vitalii/Bundle/TrackerBundle/Resources/public/js/app/components/issue-updated-at-component.js
+++ src/Vitalii/Bundle/TrackerBundle/Resources/public/js/app/components/issue-updated-at-component.js
@@ -11,6 +11,7 @@ define(function(require) {
                 widgetManager.getWidgetInstanceByAlias('tracker_issue_updated_at', function(widget) {
                     mediator.on('widget_success:note-dialog', function() {
                         widget.render();
+                        mediator.trigger('datagrid:doRefresh:issue-collaborators-grid');
                     });
                 });
             });
